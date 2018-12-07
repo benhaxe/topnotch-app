5d0fe5151384df3fe3aabadbf0902d1269f3487d
theJinxist <nerosilva522@gmail.com>
2018-12-07 01:01:00 +0100

    Added Swipe-to-delete function, a No-Request found prompt..

--- app/src/main/java/com/dscfuta/topnotch/Utils.kt
+++ app/src/main/java/com/dscfuta/topnotch/Utils.kt
@@ -1,6 +1,7 @@
 package com.dscfuta.topnotch
 
 import android.content.Context
+import android.graphics.drawable.Drawable
 import android.view.View
 import android.widget.TextView
 import androidx.core.content.ContextCompat
@@ -11,17 +12,46 @@ import com.google.android.material.snackbar.Snackbar
  */
 class Utils {
 
-//    companion object {
-//
-//        fun createCustomSnackBar(context: Context, view: View, message: String): Snackbar{
-//            val snackbar = Snackbar.make(view, message, Snackbar.LENGTH_LONG)
-//
-//            val snackbarView = snackbar.view
-//            view.setBackgroundColor(ContextCompat.getColor(context, R.color.colorPrimary))
-//            val textView = view.findViewById<TextView>(android.)
-//        }
-//
-//    }
+    companion object {
+
+        fun setColorFromText(firstLetter: String, context: Context): Drawable?{
+            return when(firstLetter){
+
+                "A" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_a)
+                "B" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_b)
+                "C" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_c)
+                "D" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_d)
+                "E" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_e)
+                "F" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_f)
+                "G" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_g)
+                "H" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_h)
+                "I" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_i)
+                "J" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_j)
+                "K" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_k)
+                "L" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_l)
+                "M" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_m)
+                "N" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_n)
+                "O" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_o)
+                "P" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_p)
+                "Q" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_q)
+                "R" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_r)
+                "S" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_s)
+                "T" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_t)
+                "U" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_u)
+                "V" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_v)
+                "W" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_w)
+                "X" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_x)
+                "Y" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_y)
+                "Z" ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_z)
+
+
+
+                else ->  ContextCompat.getDrawable(context, R.drawable.text_label_background_a)
+
+            }
+        }
+
+    }
 
 
 }
