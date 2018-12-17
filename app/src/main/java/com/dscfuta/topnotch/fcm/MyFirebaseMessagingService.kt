cd09de64ba7fb7c282dc6aafd5f0191e230c26c6
Benjamin Masebinu <haxeboom@gmail.com>
2018-12-17 18:47:16 +0100

    FCM Setup

---
+++ app/src/main/java/com/dscfuta/topnotch/fcm/MyFirebaseMessagingService.kt
@@ -0,0 +1,6 @@
+package com.dscfuta.topnotch.fcm
+
+import com.google.firebase.messaging.FirebaseMessagingService
+
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+}
---
+++ app/src/main/java/com/dscfuta/topnotch/fcm/MyJobService.kt
@@ -0,0 +1,14 @@
+package com.dscfuta.topnotch.fcm
+
+import com.firebase.jobdispatcher.JobParameters
+import com.firebase.jobdispatcher.JobService
+
+class MyJobService : JobService() {
+    override fun onStopJob(job: JobParameters?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onStartJob(job: JobParameters?): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
